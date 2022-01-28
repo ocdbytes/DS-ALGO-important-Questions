41d69526af2e671f6bfc5c6ad0a062cebea35be0
Arun Jangra <arunjngra89@gmail.com>
2022-01-28 22:51:38 +0530

    push

--- BinarySearchTree/DeleteInBST.cpp
+++ BinarySearchTree/DeleteInBST.cpp
@@ -18,10 +18,12 @@ Node* buildBST(Node* root,int val){
         return new Node(val);
     }
     if(val < root->data){
-        buildBST(root->left, val);
+        root->left = buildBST(root->left, val);
     }else{
-        buildBST(root->right,val);
+        root->right = buildBST(root->right,val);
     }
+
+    return root;
 }
 
 void printinorderBST(Node* root){
@@ -30,7 +32,7 @@ void printinorderBST(Node* root){
     }
 
     printinorderBST(root->left);
-    cout << root->data << endl;
+    cout << root->data << " ";
     printinorderBST(root->right);
 }
 
@@ -39,6 +41,61 @@ void printinorderBST(Node* root){
 // case 2 : deleting a node with one child
 // case 3 : deleting a node with two children
 
+// function to get inorder successor
+Node* iorderSuccessor(Node* root){
+    Node* curr = root;
+    while(curr && curr->left != NULL){
+        curr = curr->left;
+    }
+    return curr;
+}
+
+Node* deleteInBST(Node* root,int key){
+    // first two conditions is for finding our root node in our tree
+    if(key < root->data){
+        root->left = deleteInBST(root->left,key);
+    }
+    else if(key > root->data){
+        root->right = deleteInBST(root->right,key);
+    }else{
+        //case 1 & 2
+        if(root->left == NULL){
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        }else if(root->right == NULL){
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        //case 3
+        Node* temp = iorderSuccessor(root->right);
+        root->data = temp->data;
+        root->right = deleteInBST(root->right, temp->data);
+    }
+    return root;
+
+}
+
 int main(){
+    int n;
+    cin >> n;
+    Node* root = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        root = buildBST(root,val);
+    }
+    //printing the tree before delete
+    printinorderBST(root);
+    cout << endl;
+    int key;
+    cin >> key;
+    deleteInBST(root,key);
+    //printing the tree after delete
+    printinorderBST(root);
+    cout << endl;
+    
     return 0;
 }

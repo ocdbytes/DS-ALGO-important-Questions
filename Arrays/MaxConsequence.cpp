9beeef4b72db44b578341c177d343bc7b04e51ee
NivA <nivedita0051@gmail.com>
2022-11-11 21:34:00 +0530

    n 👾

--- Arrays/MaxConsequence.cpp
+++ Arrays/MaxConsequence.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int count = 0, max=0;
+        int count = 0, m=0;
         for (int i=0; i<nums.size(); i++){
             if (nums[i]== 1)
             {
@@ -15,12 +15,12 @@ public:
             {
                 count=0;
             }
-            if (max<=count)
+            if (m<=count)
             {
-                max= count;
+                m= count;
             }
         }   
-     return max;   
+     return m;   
         
     }
 };
